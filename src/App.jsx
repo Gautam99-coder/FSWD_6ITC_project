b95c887af1a36a945121570b7caf4f2bd9e085a4
Gautam99-coder <gautamtharu1000@gmail.com>
2025-12-26 13:55:53 +0530

    feat: Initialize project with Vite, React, and ESLint setup
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite configuration file for React support.
    - Created defaultProps component with default values and custom onClick functionality.
    - Implemented prop_example component for displaying user cards with props.
    - Added usercardcomp component to showcase user information and image.
    - Introduced CSS styles for user cards and buttons.
    - Added SVG icons for Vite and React in the public and assets directories respectively.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-
+import DefaultProps from './component/defaultProps.jsx';
 import GreetingClass from './component/ITC.jsx';
-
+import PropExample from './component/prop_example.jsx';
+import UserCard1 from './component/usercardcomp.jsx';
 function App() {
-
+  const customfunction=()=>{
+    console.log("Custom onClick executed.");
+  }
   return (
     <>
-      <GreetingClass />
-
+      {/* <GreetingClass /> */}
+      <UserCard1 />
+      <PropExample
+      img_url="https://mir-s3-cdn-cf.behance.net/project_modules/1400_webp/2ac41f74056361.5c1faf697fd88.jpg"
+      button_txt="Save" data="Gautam Bro"/>
+      <PropExample
+      img_url="https://mir-s3-cdn-cf.behance.net/project_modules/1400_webp/2ac41f74056361.5c1faf697fd88.jpg"
+      button_txt="Save" data="Gautam Bro"/>
+      <DefaultProps/>
+      <DefaultProps onclick={customfunction}/>
+      <DefaultProps text="Press Here "/>
+      <DefaultProps
+      text="Press Here "
+      onclick={customfunction}/>
     </>
   );
 }
