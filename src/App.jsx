069feeb36873d0d696889f5b09c999a2fefd4c14
Gautam99-coder <gautamtharu1000@gmail.com>
2026-01-15 23:56:38 +0530

    refactor: Clean up App component by commenting out unused imports and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import BookList from './component/Book_Management_System.jsx';
+import Calculator from './component/Calculator.jsx';
+import ContactBook from './component/contactBook.jsx';
+import ContactManager from './component/ContactManager.jsx';
+import ContactList from './component/contactNote.jsx';
+import ContactNote from './component/defaultProps.jsx';
 import DefaultProps from './component/defaultProps.jsx';
 import GreetingClass from './component/ITC.jsx';
 import PropExample from './component/prop_example.jsx';
+import TodoList from './component/todolist.jsx';
 import UserCard1 from './component/usercardcomp.jsx';
+import Calculator1 from './sir/calc.jsx';
 function App() {
-  const customfunction=()=>{
-    console.log("Custom onClick executed.");
-  }
+  // const customfunction=()=>{
+  //   console.log("Custom onClick executed.");
+  // }
   return (
     <>
-      {/* <GreetingClass /> */}
+      {/* <GreetingClass />
       <UserCard1 />
       <PropExample
       img_url="https://mir-s3-cdn-cf.behance.net/project_modules/1400_webp/2ac41f74056361.5c1faf697fd88.jpg"
@@ -22,6 +30,14 @@ function App() {
       <DefaultProps
       text="Press Here "
       onclick={customfunction}/>
+    </> */}
+
+    {/* <TodoList/> */}
+    {/* <ContactList/> */}
+    {/* <ContactManager/> */}
+    {/* <BookList/> */}
+    {/* <Calculator/> */}
+    <Calculator1/>
     </>
   );
 }
